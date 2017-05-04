c7024dbed987bb845f7416b5d823967ca7a3306f
koechlm <markus.koechl@autodesk.com>
2017-05-04 13:55:02 +0200

    Add project files.

---
+++ VDS Extension Templates/CopyTo_Default-ps1-script.ps1
@@ -0,0 +1,16 @@
+#region disclaimer
+#=============================================================================#
+# PowerShell script sample for Vault Data Standard                            #
+#                                                                             #
+# Copyright (c) Autodesk - All rights reserved.                               #
+#                                                                             #
+# THIS SCRIPT/CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER   #
+# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  #
+#=============================================================================#
+#endregion
+
+
+#region snippet for 
+# copy the conditional loop below to the <list the region or function here> in Default.ps1
+
